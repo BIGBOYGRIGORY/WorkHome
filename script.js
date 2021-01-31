57e4025b1d44367324f964507badb2c58ae1e235
Grigiriy Semenov <semenovg682@gmail.com>
2021-01-31 03:13:23 +0300

    Initial project

---
+++ script.js
@@ -0,0 +1,20 @@
+let popapObject = document.querySelector('.popap');
+let buttons = document.querySelectorAll('.main__btn');
+let btnClose = document.getElementsByClassName('popap__close')[0];
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (event) {
+        event.preventDefault();
+        popapObject.style.display = "block"
+    })
+}
+
+btnClose.onclick = function () {
+    popapObject.style.display = "none";
+}
+
+window.onclick = function () {
+    if (event.target == popapObject) {
+        popapObject.style.display = "none";
+    }
+}
